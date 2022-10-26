ed35c94d09676d1e1c0f40664726ad9d439efbdf
Morgan Rogers <morgan.rog@yahoo.com>
2022-10-26 18:49:20 -0500

    added basic UI element and fixed teleportation bug

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -12,9 +12,20 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty leftActivate;
     public InputActionProperty rightActivate;
 
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
+
     void Update()
     {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+
+        leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
